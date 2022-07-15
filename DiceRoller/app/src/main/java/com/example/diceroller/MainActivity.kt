255cf5779823938624035443fe3aa7c53b4dc82f
JD-Valenzuela <jeromedaniel.valenzuela@cvsu.edu.ph>
2022-07-15 08:41:36 +0800

    added Interactive DiceRoll

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,17 +3,30 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private fun diceRoll() {
+        val dice = Dice(6)
+        val diceRoll =  dice.roll()
+        val textviewResult: TextView = findViewById(R.id.textView)
+        textviewResult.text = diceRoll.toString()
+    }
+
+    class Dice(val diceSides: Int){
+        fun roll(): Int {
+            return (1..diceSides).random()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val diceButtonRoll: Button = findViewById(R.id. button)
-        diceButtonRoll setonClickListener {
-            val toast = Toast.makeText(this,   "Roll", Toast.LENGTH_SHORT).show()
+        val diceRollButton: Button = findViewById(R.id.button)
 
+        diceRollButton.setOnClickListener {
+            val textViewResult: TextView = findViewById(R.id.textView)
+            textViewResult.text = "6"
         }
     }
 }
