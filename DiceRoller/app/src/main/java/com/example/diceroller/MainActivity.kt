26dcd2e16abaa4d46c7fe9edbe8f5e5f7eecb9d9
JD-Valenzuela <jeromedaniel.valenzuela@cvsu.edu.ph>
2022-07-15 10:17:49 +0800

    updated diceroll to use images instead

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,27 +3,44 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    // draws dice result to textView
     private fun diceRoll() {
+        //calling dice class to roll a random number
         val dice = Dice(6)
-        val diceRoll =  dice.roll()
-        val textviewResult: TextView = findViewById(R.id.textView)
-        textviewResult.text = diceRoll.toString()
+        val diceRollCommand =  dice.roll()
+
+        //dice image display
+        val diceGraphic: ImageView =findViewById(R.id.imageView)
+        when (diceRollCommand){
+            1 -> diceGraphic.setImageResource(R.drawable.dice_1)
+            2 -> diceGraphic.setImageResource(R.drawable.dice_2)
+            3 -> diceGraphic.setImageResource(R.drawable.dice_3)
+            4 -> diceGraphic.setImageResource(R.drawable.dice_4)
+            5 -> diceGraphic.setImageResource(R.drawable.dice_5)
+            6 -> diceGraphic.setImageResource(R.drawable.dice_6)
+        }
     }
 
+    //rolls die, throws result back to die roll
     class Dice(val diceSides: Int){
         fun roll(): Int {
             return (1..diceSides).random()
         }
     }
+
+    // executed when app creates MainActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //object creation for roll button
         val diceRollButton: Button = findViewById(R.id.button)
 
+        //calls diceRoll function on button click
         diceRollButton.setOnClickListener {
             diceRoll()
         }
